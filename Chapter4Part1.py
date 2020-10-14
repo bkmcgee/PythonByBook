b1cc545140fc35194660bae9e6dcd53b35e0b2a6
bkmcgee <56489519+bkmcgee@users.noreply.github.com>
2020-10-14 17:42:31 -0400

    Create Chapter4Part1.py

---
+++ Chapter4Part1.py
@@ -0,0 +1,43 @@
+#Chapter 4 Functions
+# October 13 2020
+def f(x):
+  return x*2
+print(f(2))
+
+def func():
+  return 20*3
+result1 = func()
+print(result1)
+
+def func1(a,b,c):
+  return a+b+c
+result2 = func1(4,5,6)
+print(result2)
+
+
+age = input("Enter your age:")
+int_age = int(age)
+if int_age < 30:
+  print("wow, so young")
+else:
+  print("wow, so old")
+
+#call above age three times
+def ask_age():
+  age = input("Enter your age:")
+  int_age = int(age)
+  if int_age < 30:
+    print("wow, so young")
+  else:
+    print("wow, so old")
+
+#ask_age()
+#ask_age()
+#ask_age()
+
+#Parameters
+def adding(x1, y1=10):
+  return x1+y1
+
+result = adding(4)
+print(result)
